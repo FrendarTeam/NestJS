5ed4fc76301355d276df5c2f77c5b3876147b923
sumin-dev <bonar8888@naver.com>
2023-12-27 04:14:55 +0900

    [fix] validation pipe 옵션 추가

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,12 @@ async function bootstrap() {
     credentials: true,
   });
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
   app.useGlobalInterceptors(new LoggerAndResponseInterceptor());
 
   const config = new DocumentBuilder()
